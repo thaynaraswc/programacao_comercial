3abc34a5a97636bcec2e040a8a1a12f6791ad3f2
raimundocosta25 <raimundocostad25@gmail.com>
2016-09-02 15:28:03 -0300

    djangoProject polls

--- virtual/sistema/sistema/settings.py
+++ virtual/sistema/sistema/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'veiculos.apps.VeiculosConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- virtual/sistema/veiculos/admin.py
+++ virtual/sistema/veiculos/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from veiculos.models import *
+class VeiculoAdmin(admin.ModelAdmin):
+    """
+    Admin para o model Veiculo
+    """
+    list_display = ['marca', 'modelo', 'ano_fabricacao', 'modelo_fabricacao', 'combustivel']
+    search_fields = ['marca', 'modelo']
+    list_filter = ['combustivel']
 
-# Register your models here.
+admin.site.register(Veiculo, VeiculoAdmin)
